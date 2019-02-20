23949fdf7b52730a58ab81cdecae87bb1a6651ba
Jori Lytter <jori.lytter@gmail.com>
2019-02-20 19:55:34 +0200

    Add caching and periodic fetching of data from ruuvitags to optimise response times since reading from ruuvitags can take several seconds

--- http_api.py
+++ http_api.py
@@ -7,7 +7,7 @@ Requires:
     bottle
     ruuvitag_sensor
 '''
-import sys, json
+import sys, json, time, threading, datetime
 from bottle import get, response, request, run
 from ruuvitag_sensor.ruuvitag import RuuviTag
 from tag.tag import load_tag_configuration, format_tags_data
@@ -28,8 +28,11 @@ def enable_cors(func):
     return wrapper
 
 configuredTags = dict()
+cachedData = []
+nextRun = time.time()
 
 def read_data():
+    global cachedData
     allData = []
     for mac in configuredTags.keys():
         sensor = RuuviTag(mac)
@@ -37,26 +40,39 @@ def read_data():
         tagData = sensor.state
         tagData['name'] = configuredTags[mac]
         allData.append(tagData)
+    cachedData.clear()
     return allData
+
+def cache_data():
+    print('[{}] Refreshing cache'.format(datetime.datetime.now()))
+    global cachedData
+    global cacheTimer
+    global nextRun
+    cachePeriodSeconds = 30
+    cachedData = read_data()
+    nextRun = nextRun + cachePeriodSeconds
+    intervalInSeconds = nextRun - time.time()
+    cacheTimer = threading.Timer(intervalInSeconds, cache_data)
+    cacheTimer.start()
+
+cacheTimer = threading.Timer(0, cache_data)
 
 @get('/ruuvitag')
 @enable_cors
 def ruuvitag_data():
     response.content_type = 'application/json; charset=UTF-8'
-    return format_tags_data(read_data())
+    return format_tags_data(cachedData)
 
 if __name__ == '__main__':
-    if (len(sys.argv) > 2):
-        print('Too many arguments!')
-        sys.exit(0)
-
     # First argument is this python file itself
     if (len(sys.argv) < 2):
         print('Program needs a tag configuration file as parameter, e.g.: python http_api.py tags.json')
         sys.exit(0)
 
+    global configuredTags
     configurationFile = sys.argv[1]
     configuredTags = load_tag_configuration(configurationFile)
+    cacheTimer.start()
 
     try:
         run(host='0.0.0.0', port=5000, debug=True)
@@ -64,3 +80,5 @@ if __name__ == '__main__':
         pass
     finally:
         print('Exiting...')
+        cacheTimer.cancel()
+
